fed1e53a11fd9cf8e299bfd0f6cd54e5f2dc37c7
CarlosVeRamz <83312809+Cveramz@users.noreply.github.com>
2022-08-31 23:13:44 -0400

    Constructor de imagen y gitignore para ignorar archivos .bak

--- TDA_Image.rkt
+++ TDA_Image.rkt
@@ -1,45 +1,38 @@
 #lang scheme
 
-#|Estructuras Base del TDA:
--image
--pixbit-d
--pixrgb-d
--pixhex-d|#
-
-
 
 ;-------CONSTRUCTORES-------
 
 ;Constructor pixbit-d
+;Descripción: Pixel de bit.
 ;Entradas: x (int), y (int), bit([0|1]), depth (int)
-;Salida: Lista
+;Salida: pixrgb
 
 (define (pixbit-d x y bit depth)
   (list x y bit depth))
 
-;Constructor pixrg-d
+;Constructor pixrgb-d
+;Descripción: Pixel rgb
 ;Entradas: x (int), y (int), r (C), g (C), b (C), d (int)
 ;Observaciones de entrada: C equivale a valores entre 0 y 255.
-;Salida: Lista
+;Salida: pixrgb
 
-(define (pixrg-d x y r g b d)
+(define (pixrgb-d x y r g b d)
   (list x y r g b d))
 
 ;Constructor pixhex
 ;Entradas: x (int), y (int), hex (String), d (int)
-;Salida: List
+;Salida: pixhex
 
-(define (pixrgb x y hex d)
+(define (pixhex-d x y hex d)
   (list x y hex d))
 
 
 ;Constructor imagen
 ;Descripción: Función constructora de imagenes, la cual puede ser
 ;bitmap, hexmap o pixmap.
-;Entradas: Width (int), Height (int), Variacion (function)
-;Salida: image
+;Entradas: Width (int), Height (int), Información pixeles (lista o listas)
+;Recorrido: imagen
 
-#|(define (image Width Height Variacion)
-  )
-
-|#
+(define (image Width Height . InfPixs)
+  (list Width Height InfPixs))
